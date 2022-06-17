2c14ea4d09d099ae3c9f1de3647ae20d4fa005f7
marvinortiz22 <104816517+marvinortiz22@users.noreply.github.com>
2022-06-17 17:42:29 -0600

    Add files via upload

--- gestionAccidentes/forms.py
+++ gestionAccidentes/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .choices import Departamentos, Filtros
 from .models import DatosExtra, ReporteAccidente
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 class AccidenteForm(forms.ModelForm):
     descripcion=forms.CharField(max_length=100, widget=forms.Textarea(attrs={'rows':'3'}))
@@ -18,3 +20,14 @@ class FiltroForm(forms.Form):
     filtrar_por=forms.ChoiceField(choices=Filtros)
     filtro=forms.CharField()
 
+class loginForm(AuthenticationForm):
+    username=forms.IntegerField()
+    password: forms.CharField()
+    class Meta:
+        fields='__all__'
+
+class RegistroForm(UserCreationForm):
+    username=forms.IntegerField()
+    class Meta:
+        model=User
+        fields=['username','password1','password2']
